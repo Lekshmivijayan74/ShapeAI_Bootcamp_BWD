4067ad88a071480e642149730151f6f30fa78c67
LEKSHMI VIJAYAN <85386914+Lekshmivijayan74@users.noreply.github.com>
2021-06-27 23:50:06 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function Info(){
+  return(
+    <div class="note">
+      <h1>Javascript and React.js</h1>
+      <p>basic web dev React Js Bootcamp</p>
+      <p>This was an amazing bootcamp taken up
+        by shaurya Sinha. We covered everything
+        from Scratch including Javascript React.js,HTML.
+      </p>
+
+    </div>
+
+      
+     
+
+   
+  );
+}
+
+export default Info;
